cfd521e983f8575da417877c484980b265ec658c
David Craggs <david.craggs@tutanota.com>
2026-06-10 09:03:41 +0800

    🚨 Ignore Semgrep false positive

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 #[tokio::main]
 async fn main() {
+    // nosemgrep: rust.lang.security.args.args "Rule is to not use the first element of args, which we don't"
     let args: Vec<String> = env::args().skip(1).collect();
     gyrseek::run(args).await;
 }
